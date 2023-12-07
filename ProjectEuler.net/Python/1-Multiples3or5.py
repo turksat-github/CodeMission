e975fe78b591c00497c263eee6a21747a91aefcc
turksat-github <120672063+turksat-github@users.noreply.github.com>
2023-12-07 16:40:04 +0300

    asdf

--- ProjectEuler.net/Python/1-Multiples3or5.py
+++ ProjectEuler.net/Python/1-Multiples3or5.py
@@ -6,7 +6,7 @@ a = 3
 b = 5
 n = 1000
 sum = 0
-for i in range (n):
-    if (((i % a) == 0) or ((i % b) == 0)):
-        sum = sum + i
+for i in range (n) :
+    if (((i % a) == 0) or ((i % b) == 0)) :
+        sum += i
 print (sum)
