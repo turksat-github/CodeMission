08a6de1128b09897cf233db1d9b17d170b5c05be
turksat-github <120672063+turksat-github@users.noreply.github.com>
2023-12-07 15:27:43 +0300

    Update Multiples3or5.swift

--- ProjectEuler.net/Swift/Multiples3or5.swift
+++ ProjectEuler.net/Swift/Multiples3or5.swift
@@ -6,7 +6,7 @@ var a = Int (3)
 var b = Int (5)
 var n = Int (1000)
 var sum = Int (0)
-for i in stride (from:1, to:n, by: 1) {
+for i in stride (from : 1, to : n, by : 1) {
     if (((i % a) == 0) || ((i % b) == 0)) {
         sum += i
     }
