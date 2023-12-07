5c8dbbf08064e941e13895314d7d5c6a91fa89ea
turksat-github <120672063+turksat-github@users.noreply.github.com>
2023-12-07 16:38:52 +0300

    sdfds

---
+++ ProjectEuler.net/Swift/2-FibonacciSumEvenNubers.swift
@@ -0,0 +1,18 @@
+/*
+sum of the even-valued terms of
+Fibonacci sequence under four million
+1, [2], 3, 5, [8], 13, 21, [34], 55, 89, [144]...
+*/
+var a = Int (1)
+var b = Int (2)
+var n = Int (4000000)
+var sum = Int (0)
+while (a < n) {
+    if (b % 2 == 0) {
+        sum += b
+    }
+    let c = a
+    a = b
+    b += c
+}
+print ("\(sum)")
