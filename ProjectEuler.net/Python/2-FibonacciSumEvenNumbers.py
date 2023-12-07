61df91812b44759e1f4165330e6e64a4cb4a6447
turksat-github <120672063+turksat-github@users.noreply.github.com>
2023-12-07 16:42:32 +0300

    sfdsf

---
+++ ProjectEuler.net/Python/2-FibonacciSumEvenNumbers.py
@@ -0,0 +1,16 @@
+'''
+sum of the even-valued terms of
+Fibonacci sequence under four million
+1, [2], 3, 5, [8], 13, 21, [34], 55, 89, [144]...
+'''
+a = 3
+b = 5
+n = 4000000
+sum = 0
+while (b < n) :
+    if ((b % 2) == 0) :
+        sum += b
+    c = a
+    a = b
+    b += c
+print (sum)
