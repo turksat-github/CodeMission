7fcf60353b3cb9aa424ac8ddbdfb72562878f23b
turksat-github <120672063+turksat-github@users.noreply.github.com>
2023-12-07 15:26:04 +0300

    Problem1

--- ProjectEuler.net/Python/1-Multiples3or5.py
+++ ProjectEuler.net/Python/1-Multiples3or5.py
@@ -4,8 +4,9 @@ sum all of the correct numbers until 1000
 '''
 a = 3
 b = 5
+n = 1000
 sum = 0
-for i in range (1000):
+for i in range (n):
     if (((i % a) == 0) or ((i % b) == 0)):
         sum = sum + i
 print (sum)
