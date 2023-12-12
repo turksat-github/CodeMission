485cca3938dfadb591f6263fa68983c7cd3d0aaf
turksat-github <120672063+turksat-github@users.noreply.github.com>
2023-12-12 07:39:41 +0300

    Problem4 swift update

--- ProjectEuler.net/Swift/04-SumSquareDifference.swift
+++ ProjectEuler.net/Swift/04-SumSquareDifference.swift
@@ -7,7 +7,7 @@ the square of the sum of the first ten natural numbers
 let n = 100
 var sum1 = Int(0), sum2 = Int(0)
 for i in 1...n {
-    sum1 += i*i
+    sum1 += i * i
 }
 for i in 1...n {
     sum2 += i
