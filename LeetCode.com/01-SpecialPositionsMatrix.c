c5d55636bdc937f4aae33b244542b8c369fa6025
turksat-github <turksat@outlook.com>
2023-12-21 16:11:51 +0300

    LeetCode.com 01

---
+++ LeetCode.com/01-SpecialPositionsMatrix.c
@@ -0,0 +1,10 @@
+int numSpecial(int **mat, int matSize, int *matColSize) {
+    int i, j, result;
+    for (i = 0; i < matSize; ++ i) {
+        for (j = 0; j < *(matColSize + i); ++ j) {
+            printf ("%d\t", *(*(mat + i) + j));
+        }
+        printf ("\n");
+    }
+    return 0;
+}
