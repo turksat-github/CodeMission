0a8bf0b462895e732e5fa56906b62c29e5859b32
turksat-github <turksat@outlook.com>
2023-12-21 15:34:14 +0300

    main for HackerRank

---
+++ HackerRank.com/Python/02-simpleArraySum.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+'''
+Complete the 'simpleArraySum' function below.
+The function is expected to return an INTEGER.
+The function accepts INTEGER_ARRAY ar as parameter.
+'''
+
+def simpleArraySum (ar) :
+    #Write your code here
+    ar_total = 0
+    for x in ar :
+        ar_total += x
+    return ar_total
+
+if __name__ == '__main__' :
+    fptr = open (os.environ ['OUTPUT_PATH'], 'w')
+    ar_count = int (input ().strip ())
+    ar = list (map (int, input ().rstrip ().split ()))
+    result = simpleArraySum (ar)
+    fptr.write (str (result) + '\n')
+    fptr.close ()
