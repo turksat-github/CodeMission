0aed84190842132767afc98bc4552129842b07b3
turksat-github <turksat@outlook.com>
2023-12-28 07:00:45 +0300

    Swift Experience

---
+++ Swift/Experience.swift
@@ -0,0 +1,41 @@
+var convertedNumber:Int?
+convertedNumber = 33
+print (convertedNumber!)
+if var convertedNumber = convertedNumber {
+    convertedNumber = 44
+    print (convertedNumber)
+}
+
+
+print (UInt16.max)
+let http404Error = ("Not Found", 404, "ali")
+let (_, justTheStatusCode, _) = http404Error
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+}
+var square = Rect(origin: Point(x: 0.0, y: 0.0),
+    size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+// initialSquareCenter is at (5.0, 5.0)
+square.center = Point(x: 15.0, y: 15.0)
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+// Prints "square.origin is now at (10.0, 10.0)"
