0ac51eee99a3dd3b6aee170b3ee2b156aca8c405
turksat-github <turksat@outlook.com>
2024-02-17 23:20:17 +0300

    double pointer update

--- C/Experience/arrayC.c
+++ C/Experience/arrayC.c
@@ -1,34 +1,32 @@
-// C Program to show allocation of data items
-// in multidimensional array
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main (int argc, char **argv, char **envp) {
-    // Declaring the dimension of 2-Dimension array
     int row = 2;
     int col = 5;
 
-    // Creating an array arr with size row * col
-    int *arr = (int *)malloc (row * col * sizeof (int));
+    int *arr = (int *) malloc (row * col * sizeof (int));
     int k = 0;
 
-    // Inserting values in the array
     for (int i = 0; i < row; ++ i) {
         for (int j = 0; j < col; ++ j) {
-            // The memory address of arr [i] [j]
-            // is equal to (arr + i * col + j)
-            *(arr + i * col + j) = k;
+            *(arr + i * col + j) = k;// arr [i] [j]
             ++ k;
         }
     }
 
-    // Printing the multidimensional array
     for (int i = 0; i < row; ++ i) {
         for (int j = 0; j < col; ++ j) {
-            printf ("%d ", *(arr + i * col + j));
+            printf ("%d ", *(arr + i * col + j));// arr [i] [j]
         }
         printf ("\n");
     }
-    return EXIT_SUCCESS;
+
+    //Code for further processing and free the dynamically allocated memory
+    for (i = 0; i < row; ++ i) {
+        free (arr [i]);
+    }
+    free (arr);
+
+  return EXIT_SUCCESS;
 }
